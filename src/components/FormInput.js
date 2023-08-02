7e909de2a83dbd7dd51eb4124bd5d6bb93e91b1d
Ceygl <gulceyda98@gmail.com>
2023-08-02 10:31:49 +0200

    warning window created.

--- src/components/FormInput.js
+++ src/components/FormInput.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import "../index.css"
 import Button from './Button';
+import WarningWindow from './WarningWindow';
 
 const FormInput = () => {
-    const [userName, setUserName] = useState();
-    const [ageInput, setAgeInput] = useState();
+    const [userName, setUserName] = useState('');
+    const [ageInput, setAgeInput] = useState('');
     const [usersList, setUsersList] = useState([]);
     const [showWarning, setShowWarning] = useState(false);
 
@@ -20,7 +21,7 @@ const FormInput = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+
         if (userName.trim() === '' || isNaN(ageInput) || ageInput <= 0) {
             setShowWarning(true);
             return;
@@ -38,6 +39,7 @@ const FormInput = () => {
     };
     return (
         <div className='container'>
+            {showWarning && <WarningWindow onClose={() => setShowWarning(false)} />}
             <form onSubmit={handleSubmit}>
                 <label htmlFor="input1">Username</label>
                 <input
@@ -59,16 +61,16 @@ const FormInput = () => {
                 <Button />
             </form>
             {usersList.length > 0 && (
-            <div className='container added'>
-                
+                <div className='container added'>
+
                     {usersList.map((user, index) => (
-                    <div key={index}>
-                        <p>{user.username} ({user.age})</p>
-                    </div>
-                ))}
-                
-                
-            </div>
+                        <div key={index}>
+                            <p>{user.username} ({user.age})</p>
+                        </div>
+                    ))}
+
+
+                </div>
             )}
         </div>
     )
--- src/components/WarningWindow.js
+++ src/components/WarningWindow.js
@@ -4,8 +4,8 @@ import React from 'react';
 const WarningWindow = ({ onClose }) => {
     return (
         <div className="warning-window">
-            <p>Warning: Please fill in a valid username and age (greater than 0).</p>
-            <button onClick={onClose}>Close</button>
+            <p>Please enter a valid username and age!</p>
+            <button onClick={onClose}>Ok</button>
         </div>
     );
 };
